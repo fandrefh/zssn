cf8e9990bcc92d7815be0c1151df2dc90490fbaa
Francisco André <fandrefh@gmail.com>
2020-01-28 17:36:33 -0300

    Deploy inicial

--- zssn/settings.py
+++ zssn/settings.py
@@ -140,8 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Rest Framework configs
 
